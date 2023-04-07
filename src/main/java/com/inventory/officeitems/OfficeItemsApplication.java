31a8da034dd17ac322589100db0aa33df9b24a03
RohuTheCoder <rohugb777999@gmail.com>
2023-04-07 16:45:35 -0700

    Initial Commit

---
+++ src/main/java/com/inventory/officeitems/OfficeItemsApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.officeitems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeItemsApplication.class, args);
+	}
+
+}
